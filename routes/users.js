e7c81d2f2000b99e4ffe2a8543e36e7e8408a0fc
quocdatend <quocdatend>
2025-04-10 22:56:02 +0700

    update reset pass user

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var userControllers = require('../controllers/users')
-let { check_authentication, check_authorization } = require("../utils/check_auth");
+let { check_authentication, check_authorization, check_authenticationAdmin, check_authorizationAdmin } = require("../utils/check_auth");
 const constants = require('../utils/constants');
 let { validate, UserValidation } = require('../utils/validator')
 
-router.get('/', check_authentication, check_authorization(constants.USER_PERMISSION), UserValidation, validate, userControllers.getAllUsers);
+router.get('/', check_authenticationAdmin, check_authorizationAdmin(constants.ADMIN_PERMISSION), UserValidation, validate, userControllers.getAllUsers);
 
 router.post('/', UserValidation, validate, userControllers.createAnUser);
 
 router.put('/:id',check_authentication, check_authorization(constants.USER_PERMISSION), UserValidation, validate, userControllers.updateAnUser);
 
-router.delete('/:id', check_authentication, check_authorization(constants.ADMIN_PERMISSION), userControllers.deleteAnUser);
+router.delete('/:id', check_authenticationAdmin, check_authorizationAdmin(constants.ADMIN_PERMISSION), userControllers.deleteAnUser);
 
 
 
